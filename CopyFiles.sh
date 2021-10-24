a9c8c0015ef2d4c3d4b7fbc3d711bf3c7f368f56
Aaron Daws <dawsdev@outlook.com>
2021-10-24 18:56:09 +1000

    Update CopyFiles.sh

--- CopyFiles.sh
+++ CopyFiles.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
-cp ~/Temp/\#LocalGitsRepos/amdaws.github.io ~/Temp/\playground
+#timesetting-https://www.gnu.org/software/coreutils/manual/html_node/date-invocation.html
+now=$(date +"%Y%m%d_%H%M")
+cp -r ~/Temp/\#LocalGitsRepos ~/QPE\ BAS/Information\ Technology\ Services\ -\ General/\GitBackup_$now
+#cp -r ~/Temp/\#LocalGitsRepos ~/Temp/\playground
+#cp -r ~/Temp/\#LocalGitsRepos ~/Temp/\playground
 
 echo "Done!"
 
